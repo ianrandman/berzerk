c073ec2923387a12a61e8f67a16169003127a8ab
Ian Randman <ixr5487@cs.glados.rit.edu>
2019-03-03 00:20:08 -0500

    readme

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ###Berzerk AI Agents
 This is an implementation of 3 different agents in addition to a random agent that can all play the Atari game Berzerk.
+Install the necessary imports at the top of the files.
 
 ###Reflex Agent
-This is an implementation of the A\* algorithm to play the game. To run it //TODO
+This is an implementation of the reflex algorithm to play the game. To run it, change the agent in line 45 of gym_play.py to ReflexAgent.
 
 ###NEAT Agent
-This is an implementation of the Neuroevolution of Augmented Topologies algorithm to play the game. To run it //TODO
+This is an implementation of the Neuroevolution of Augmented Topologies algorithm to play the game. To run it, change the agent in line 45 of gym_play.py to NEATAgent.
 
 ###Random Agent
-This is an agent that just makes random moves. To run it //TODO
+This is an agent that just makes random moves. To run it, change the agent in line 45 of gym_play.py to RandomAgent.
--- gym-play.py
+++ gym-play.py
@@ -30,43 +30,39 @@ if __name__ == '__main__':
 
     # You can set the level to logger.DEBUG or logger.WARN if you
     # want to change the amount of output.
-    # logger.set_level(logger.INFO)
+    logger.set_level(logger.INFO)
 
-    print("run number, score, number of levels, total steps, total elapsed time")
+    env = gym.make(args.env_id)
 
-    for i in range(100):
-        env = gym.make(args.env_id)
-
-        # You provide the directory to write to (can be an existing
-        # directory, including one with existing data -- all monitor files
-        # will be namespaced). You can also dump to a tempdir if you'd
-        # like: tempfile.mkdtemp().
-        outdir = 'random-agent-results'
+    # You provide the directory to write to (can be an existing
+    # directory, including one with existing data -- all monitor files
+    # will be namespaced). You can also dump to a tempdir if you'd
+    # like: tempfile.mkdtemp().
+    outdir = 'random-agent-results'
 
 
-        env.seed(0)
-        agent = RandomAgent(env.action_space)
+    env.seed(0)
+    agent = ReflexAgent(env.action_space)
 
-        episode_count = 100
-        reward = 0
-        done = False
-        score = 0
-        special_data = {}
-        special_data['ale.lives'] = 3
-        ob = env.reset()
-        while not done:
+    episode_count = 100
+    reward = 0
+    done = False
+    score = 0
+    special_data = {}
+    special_data['ale.lives'] = 3
+    ob = env.reset()
+    while not done:
 
-            action = agent.act(ob, reward, done)
-            ob, reward, done, x = env.step(action)
-            score += reward
-            # env.render()
+        action = agent.act(ob, reward, done)
+        ob, reward, done, x = env.step(action)
+        score += reward
+        env.render()
 
-        agent.act(ob, reward, done)
+    agent.act(ob, reward, done)
 
-        agent_num_levels = agent.num_levels
-        agent_total_steps = agent.total_steps
-        agent_elapsed_time = agent.elapsed_time
-        # Close the env and write monitor result info to disk
-        #print ("Your score: %d" % score)
-        print(str(i+1)+', '+str(score)+', '+str(agent_num_levels)+', '+str(agent_total_steps)+', '+str(agent_elapsed_time))
-        env.close()
+    agent_num_levels = agent.num_levels
+    agent_total_steps = agent.total_steps
+    agent_elapsed_time = agent.elapsed_time
+    # Close the env and write monitor result info to disk
+    print ("Your score: %d" % score)
+    env.close()
